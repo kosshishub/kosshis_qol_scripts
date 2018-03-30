bf4bb5cb653ba3ac513f89842bf332915441b61a
kosshishub <kosshishub@users.noreply.github.com>
2018-03-30 18:48:44 +0300

    Update plugin

--- chrome-plugin/popup.js
+++ chrome-plugin/popup.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const session_json_header = "/*BROWSER_SESSION_JSON_KOSSHI_FI*/";
+const session_json_help = 
+`
+/*
+* Do not remove the comment above! Removing it will cause the extension to treat
+* this file as just urls seprated with newlines!
+* 
+* # File format
+*
+* Basic JSON. Root is an array of window objects. A window object is another 
+* array with urls in it. Thats it. 
+* Example: 
+* [ ["opens in window 1"], ["opens in window 2", "also opens in win2"] ]
+*/
+
+`;
+
+const commentStripper = new CommentStripper();
 
 function allTabs(callback) {
 	chrome.tabs.query({}, function(tabs) {
@@ -14,104 +32,50 @@ function currentWindowTabs(callback){
 		});
 	});
 }
-
-function main(){
-	let txt = document.querySelector('#text');
-	txt.innerHTML = 
-	`
-	<button id="closeDupesFromAll">    Close duplicate tabs from everywhere</button><br>
-	<button id="closeDupesFromCurrent">Close duplicate tabs from current window</button><br>
-	
-	<button id="logDupes">Log dupes in current</button>
-	<button id="saveTabs">Log all tabs in current</button>
-	<br>Open links from a txt file: <input id="file" type="file"/>
-	<br>Log:
-	<div id="log"></div>
-	`;
-
-	document.querySelector('#closeDupesFromAll').onclick     = ()=>{
-
-	};
-	document.querySelector('#closeDupesFromCurrent').onclick = ()=>{
-		currentWindowTabs((tabs)=>{
-			getDupeTabs(tabs, (deletionIDList, uniqueCount)=>{
-				console.log(deletionIDList);
-				print(`Closing ${deletionIDList.length} tabs...`);
-				chrome.tabs.remove(deletionIDList, ()=>print('Success!'));
-			});
+function currentTab(callback){
+	chrome.windows.getCurrent((w)=>{ 
+		chrome.tabs.query({windowId:w.id, active:true}, (a)=>{
+			callback(a);
 		});
-	};
-
-	document.querySelector('#saveTabs').onclick     = ()=>{
-		currentWindowTabs((tabs)=>{
-			let file = "";
-			for (var i = 0; i < tabs.length; i++) {
-				file += tabs[i].url + "<br>";
-			}
-			print(file);
-
-		});
-	};
-
-	document.querySelector('#logDupes').onclick = ()=>{
-		// kill me pls
-		currentWindowTabs((tabs)=>{
-			getDupeTabs(tabs, (deletionIDList, uniqueCount, delURL, duplicateTabIDs)=>{
-				duplicateTabIDs.forEach((id)=>{
-					chrome.tabs.get(id, (tab)=>{
-						print(`<button id="tab${tab.id}select">select</button> <button id="tab${tab.id}close">close</button> ${tab.url}`);
-						document.querySelector( `#tab${tab.id}select` ).addEventListener('click',()=>{
-							chrome.tabs.update(tab.id, {selected:true});
-						});
-						document.querySelector( `#tab${tab.id}close` ).addEventListener('click',()=>{
-							chrome.tabs.remove([tab.id]);
-						});
-					});
-				});
-			});
-		});
-	};
-
-	document.querySelector('input#file').addEventListener('change', ()=>{
-		let file = document.querySelector('input#file').files;
-		let fn = new FileReader();
-
-		fn.onload = function(event) {
-			let list = event.target.result.split('\n');
-			let cfg = {url:[]};
-
-			for (var i = 0; i < list.length; i++) 
-				if(list[i].match(/http/)) cfg.url.push( list[i] );
-
-			chrome.windows.create(cfg);
-		};
-
-		fn.readAsText(file[0]);
 	});
-
-	chrome.runtime.onMessage.addListener( (request, sender)=>{
-		if (request.action == "twitterMedia") {
-			print('Opened: '+request.source);
-			chrome.tabs.create({url:request.source, active:false});
+}
+function httpget(url, callback){
+	let xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+			callback(this.responseText);
 		}
-	});
-	chrome.tabs.executeScript(null, {
-		file: "twittermedia.js"
-	});
-		
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
+}
 
+function fixurl(url){
+	let domain = /:\/\/(.*?)\//.exec(url)[1];
+	if(domain == "klbibkeccnjlkjkiokjodocebajanakg") 
+		url = /uri=(.*)/.exec(url)[1];
+	return url;
+}
 
+function downloadText(text){
+	var blob = new Blob([text], {type: "text/plain"});
+	var url = URL.createObjectURL(blob);
+	chrome.downloads.download({
+	  url: url // The object URL can be used as download URL
+	  
+	});
 
-	currentWindowTabs((tabs)=>{
-		getDupeTabs( tabs, (deletionIDList, uniqueCount)=>{
-			print(`In current window:<br> ${tabs.length} tabs, ${deletionIDList.length} dupes and ${uniqueCount} uniques.`);
-		});
-	});
-	allTabs((tabs)=>{
-		getDupeTabs( tabs, (deletionIDList, uniqueCount)=>{
-			print(`In all windows:<br> ${tabs.length} tabs, ${deletionIDList.length} dupes and ${uniqueCount} uniques.`);
-		});
-	});
+}
+ 
+function selectTab(id){
+	chrome.tabs.update(id, {selected:true});
+
+	chrome.tabs.get(id, (tab)=>{
+		// windowID
+		// console.log(tab);
+		chrome.windows.update(tab.windowId, {focused:true})
+
+	})
 
 }
 
@@ -151,6 +115,227 @@ function getDupeTabs( tabs, callback ){
 	}
 
 	callback( deletionIDList, num, delURL, duplicateTabIDs );
+}
+
+function createTabButton(tab){
+
+	let result = document.createElement('div');
+
+	let selectButton = document.createElement('button');
+	selectButton.className = "searchResult";
+	selectButton.innerHTML = "<b>"+tab.title+"</b><br><i>" + tab.url + "</i>";
+	selectButton.onclick = ()=>{ console.log('select', tab.id); selectTab( tab.id ); };
+	result.appendChild(selectButton);
+
+	return result;
+
+}
+
+function main(){
+	let txt = document.querySelector('#text');
+	txt.innerHTML = 
+	`
+
+	<h2>Tab search</h2>
+	<input type="text" id="tabSearch" placeholder="Search tabs"><br>
+	<div id="tabSearchResults"></div>
+	
+	<h2>Misc</h2>
+	<button id="logDupes">Show duplicate tabs</button>
+	<button id="closeDupesFromAll">    Close duplicate tabs from everywhere</button><br>
+	<button id="closeDupesFromCurrent">Close duplicate tabs from current window</button><br>
+	<button id="listDomain">List by domain</button>
+
+	<h2>Session managment</h2>
+	<button id="sessionSaveCurrent">Download session file (Window)</button><br>
+	<button id="sessionSaveGlobal" >Download session file (Global)</button>
+	<br>Import session file: <input id="file" type="file"/>
+	<br>
+	<br>Log:
+	<div id="log"></div>
+	`;
+
+
+	document.querySelector('#tabSearch').oninput = ()=>{
+		let elem = document.querySelector('#tabSearch');
+		let resultElem = document.querySelector('#tabSearchResults');
+
+		let term = elem.value.toUpperCase();
+		resultElem.innerHTML = '';
+
+		if(!term) {
+			return;
+		}
+
+		allTabs( (tabs)=>{
+			for (var i = 0; i < tabs.length; i++) {
+				let tab = tabs[i]
+				if( tabs[i].title.toUpperCase().search(term) > 0 || 
+					tabs[i].url.toUpperCase().search(term) > 0
+				) {
+					resultElem.appendChild(createTabButton(tab));
+				}
+			}
+		} );
+
+
+
+
+	}
+
+	document.querySelector('#listDomain').onclick     = ()=>{
+		let domains = {};
+		let total = 0;
+		allTabs((tabs)=>{
+
+			for (var i = 0; i < tabs.length; i++) {
+				let key = /:\/\/(.*?)\//.exec(tabs[i].url)[1];
+				if(key == "klbibkeccnjlkjkiokjodocebajanakg") 
+					key = /uri=.*:\/\/(.*?)\//.exec(tabs[i].url)[1];
+				domains[key]=++domains[key]||1;
+				total++;
+				console.log(key);
+			}
+			for(let key in domains)
+				print(`${domains[key]} (${Math.round(domains[key]/total*100)}%): ${key}`);
+			
+		});
+	};
+
+	document.querySelector('#closeDupesFromCurrent').onclick = ()=>{
+		currentWindowTabs((tabs)=>{
+			getDupeTabs(tabs, (deletionIDList, uniqueCount)=>{
+				console.log(deletionIDList);
+				print(`Closing ${deletionIDList.length} tabs...`);
+				chrome.tabs.remove(deletionIDList, ()=>print('Success!'));
+			});
+		});
+	};
+
+	document.querySelector('#sessionSaveCurrent').onclick     = ()=>{
+		currentWindowTabs((tabs)=>{
+			let json = [[]];
+			for (var i = 0; i < tabs.length; i++) {
+				json[0].push(fixurl(tabs[i].url));
+			}
+			downloadText(
+				session_json_header +
+				session_json_help +
+				JSON.stringify(json, null, "\t")
+			);
+
+		});
+	};
+
+	document.querySelector('#sessionSaveGlobal').onclick     = ()=>{
+		allTabs((tabs)=>{
+
+			let windows = {};
+
+			for (var i = 0; i < tabs.length; i++) {
+				let winkey = "win"+tabs[i].windowId;
+				if(windows[winkey] === undefined) windows[winkey] = [];
+				windows[winkey].push(fixurl(tabs[i].url));
+			}
+
+			let json = [];
+			for (let key in windows) {
+				json.push(windows[key]);
+			}
+			downloadText(
+				session_json_header +
+				session_json_help +
+				JSON.stringify(json, null, "\t")
+			);
+
+		});
+	};
+
+	// document.querySelector('#sessionSaveCurrent').onclick     = ()=>{
+	// 	currentWindowTabs((tabs)=>{
+	// 		let file = "";
+	// 		for (var i = 0; i < tabs.length; i++) {
+	// 			file += fixurl(tabs[i].url) + "\n";
+	// 		}
+	// 		downloadText(file);
+
+	// 	});
+	// };
+
+	document.querySelector('#logDupes').onclick = ()=>{
+		
+
+		console.log("0");
+
+		currentWindowTabs((tabs)=>{
+			console.log("1");
+			getDupeTabs(tabs, (deletionIDList, uniqueCount, delURL, duplicateTabIDs)=>{
+
+				console.log("2");
+				duplicateTabIDs.forEach((id)=>{
+
+					console.log("3");
+					chrome.tabs.get(id, (tab)=>{
+						document.querySelector("#tabSearchResults").appendChild(createTabButton(tab));
+					});
+				});
+			});
+		});
+	};
+
+	document.querySelector('input#file').addEventListener('change', ()=>{
+		let file = document.querySelector('input#file').files;
+		let fn = new FileReader();
+
+		fn.onload = function(event) {
+			let text = event.target.result;
+			let list = text.split('\n');
+			if(list[0] == session_json_header) {
+				let session = JSON.parse(commentStripper.strip(text));
+
+				console.log(session);
+
+				for (var i = 0; i < session.length; i++) 
+					chrome.windows.create( { url:session[i] } );
+				
+			} else {
+
+				let cfg = {url:[]};
+
+				for (var i = 0; i < list.length; i++) 
+					if(list[i].match(/http/)) cfg.url.push( list[i] );
+
+				chrome.windows.create(cfg);
+			}
+		};
+
+		fn.readAsText(file[0]);
+	});
+
+	chrome.runtime.onMessage.addListener( (request, sender)=>{
+		if (request.action == "twitterMedia") {
+			print('Opened: '+request.source);
+			chrome.tabs.create({url:request.source, active:false});
+		}
+	});
+	chrome.tabs.executeScript(null, {
+		file: "twittermedia.js"
+	});
+		
+
+
+
+	currentWindowTabs((tabs)=>{
+		getDupeTabs( tabs, (deletionIDList, uniqueCount)=>{
+			print(`In current window:<br> ${tabs.length} tabs, ${deletionIDList.length} dupes and ${uniqueCount} uniques.`);
+		});
+	});
+	allTabs((tabs)=>{
+		getDupeTabs( tabs, (deletionIDList, uniqueCount)=>{
+			print(`In all windows:<br> ${tabs.length} tabs, ${deletionIDList.length} dupes and ${uniqueCount} uniques.`);
+		});
+	});
+
 }
 
 document.addEventListener('DOMContentLoaded', main);
